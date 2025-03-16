1264b19f193defc0f588e18dab1c1f54bdbfd2ca
PhamDinhNgan <169060597+NganPD@users.noreply.github.com>
2025-03-16 12:31:54 +0700

    Health UI

---
+++ Assets/Scripts/Player/Health.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float startingHealth;
+    public float CurrentHealth { get; private set; }
+    private Animator anim;
+    private bool dead;
+
+    private void Awake()
+    {
+        CurrentHealth = startingHealth;
+    }
+
+    public void TakeDamage(float _damage)
+    {
+        CurrentHealth = Mathf.Clamp(CurrentHealth - _damage, 0, startingHealth);
+
+        if (CurrentHealth > 0)
+        {
+            //player hurt
+            anim.SetTrigger("Hurt");
+        }
+        else
+        {
+            //player dead
+            if (!dead)
+            {
+                anim.SetTrigger("Death");
+                GetComponent<Player>().enabled = false;
+                dead = true;
+            }
+            
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            TakeDamage(1);
+        }
+    }
+}
